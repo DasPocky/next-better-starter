9cb50171b0388720f9fed0c25f3bbe2d54395106
Björn End <bjoern@endmail.de>
2025-04-29 18:01:43 +0200

    feat(db): SQL-Skripte für Tabellenstruktur und Constraints hinzugefügt

--- drizzle/0000_nasty_fenris.sql
+++ drizzle/0000_nasty_fenris.sql
@@ -30,10 +30,10 @@ CREATE TABLE "users" (
 	"id" text PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
 	"email" text NOT NULL,
-	"email_verified" boolean NOT NULL,
+	"email_verified" boolean DEFAULT false NOT NULL,
 	"image" text,
-	"created_at" timestamp NOT NULL,
-	"updated_at" timestamp NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
 --> statement-breakpoint
--- drizzle/0001_opposite_agent_zero.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "users" ALTER COLUMN "email_verified" SET DEFAULT false;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "created_at" SET DATA TYPE timestamp with time zone;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "created_at" SET DEFAULT now();--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "updated_at" SET DATA TYPE timestamp with time zone;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "updated_at" SET DEFAULT now();
